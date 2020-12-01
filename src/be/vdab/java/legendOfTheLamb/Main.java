79e82ede604ec22bc7732f1726e6a1096a585bea
BrechtVanHecke <vanhecke.b@outlook.com>
2020-12-01 09:23:00 +0100

    menu

--- src/be/vdab/java/legendOfTheLamb/Main.java
+++ src/be/vdab/java/legendOfTheLamb/Main.java
@@ -1,7 +1,21 @@
 package be.vdab.java.legendOfTheLamb;
 
+
+import be.vdab.java.legendOfTheLamb.menu.Menu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int i = 5;
+        Scanner keyboard = new Scanner(System.in);
+        Menu game = new Menu();
+
+        game.raceMenu();
+        String race = keyboard.next();
+        game.classMenu();
+        String characterClass = keyboard.next();
+
+        System.out.println(race + ", " +characterClass);
+
     }
 }
---
+++ src/be/vdab/java/legendOfTheLamb/menu/Menu.java
@@ -0,0 +1,41 @@
+package be.vdab.java.legendOfTheLamb.menu;
+
+public class Menu {
+    public void startScreen(){
+        System.out.println("+++++++++++++++++++++++++++++");
+        System.out.println("++ Legend of the Lamb - DA ++");
+        System.out.println("+++++++++++++++++++++++++++++");
+        System.out.println("--    DA Studios Belgium   --");
+        System.out.println("-----------------------------");
+        System.out.println("Loading Maps and Saved games");
+        System.out.println("++++++++++");
+        System.out.println("++ DONE ++");
+        System.out.println("++++++++++");
+    }
+    public void mainMenu(){
+        System.out.println("What do you want to do");
+        System.out.println("New      - Start a new game");
+        System.out.println("Load     - Load a saved game");
+        System.out.println("Reset    - Reset all saved files");
+        System.out.println("Controls - Game controls");
+        System.out.println("Settings - Game settings");
+        System.out.println("Quit     - Quit game");
+    }
+    public void newGame(){
+        System.out.println("What map do you want to play?");
+        System.out.println("   1. Forest of Streams");
+        System.out.println("   2. Cave of Thread");
+    }
+    public void raceMenu(){
+        System.out.println("What race do you want to play with?");
+        System.out.println("Human");
+        System.out.println("Dwarf");
+        System.out.println("Elf");
+    }
+    public void classMenu(){
+        System.out.println("What class do you want to play with?");
+        System.out.println("Fighter");
+        System.out.println("Ranger");
+        System.out.println("Healer");
+    }
+}
